7c219466aeabcac72720da2e2c662ec0ed373fa3
Alee Dhillon <ali.asghar@invozone.com>
2022-05-05 19:21:13 +0500

    basic vue app is set

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="display-1 text-center">ScreenShoter</h1>
+
+    <div class="container">
+      <div class="col-col-md-4" v-for="image in images" :key="image.id">
+        <img src="image.src" alt="screenshot">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      images: []
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
